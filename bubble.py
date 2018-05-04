b4ab108a36b48c52ccae0b60cb3be287133868ac
bibinbin <bibinbin@360.net>
2018-05-04 08:10:24 +0800

    change bubble.py

--- bubble.py
+++ bubble.py
@@ -1,8 +1,9 @@
 
+
 def bubble_sort(list):
 	count = len(list)
 	for i in range(0,count):
-		for j in range(i+1,list):
+		for j in range(i+1,count):
 			if list[i]>list[j]:
 				list[i],list[j]=list[j],list[i]
 	return list
